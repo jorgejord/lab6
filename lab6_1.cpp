26956472350f343b7830998a4a6fceb775cc6bb2
Jorge <57448609+jorgejord@users.noreply.github.com>
2025-01-14 11:34:16 +0700

    Update lab6_1.cpp
    Forgot to enter the first code

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int n ;
+    int even = 0,odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n ;
+
+    while(n != 0){
+        if (n % 2 == 0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+
+    cout << "Enter an integer: ";
+    cin >> n ;
+    }
+    cout << "#Even numbers = "<< even<<"\n" ;
+    cout << "#Odd numbers = " << odd ;
     return 0;
 }
